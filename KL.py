190a99d134a95adf2028350f0119d9edc20a31e5
Daniel Liu <45900240+ltyLittlePrince@users.noreply.github.com>
2020-05-12 03:38:41 +0800

    Submitted code for Problem 10.2(c)

--- KL.py
+++ KL.py
@@ -7,11 +7,11 @@ def KL(p,q):
 ABC = [[1/2,1/2], [1/4,3/4], [1/8,7/8]]
 A, B, C = ABC[0], ABC[1], ABC[2]
 
-# verify (a) and (c)
+# verify (i) and (iii)
 for i in range(0,3):
   for j in range(0,3):
     print(KL(ABC[i], ABC[j]))
 
-# reject (b) and (d)
+# reject (ii) and (iv)
 print(KL(A,B),KL(B,A))
 print(KL(A,B)+KL(B,C),KL(A,C))
